4cd627753cfcd02a9328a49d1d3e1ef7ba38ebab
Jean Griesbacher <jnsmplcpt@gmail.com>
2020-05-21 21:58:09 +0200

    Lombock example

--- src/main/java/guru/springframework/msscbrewery/web/controllers/CustomerController.java
+++ src/main/java/guru/springframework/msscbrewery/web/controllers/CustomerController.java
@@ -3,6 +3,8 @@ package guru.springframework.msscbrewery.web.controllers;
 import com.sun.net.httpserver.Headers;
 import guru.springframework.msscbrewery.web.model.CustomerDto;
 import guru.springframework.msscbrewery.web.services.CustomerService;
+import lombok.RequiredArgsConstructor;
+import lombok.val;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,16 +12,13 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.UUID;
 
+@RequiredArgsConstructor
 @RestController
 @RequestMapping(CustomerController.BASE_URL)
 public class CustomerController {
 
     public static final String BASE_URL = "/api/v1/customers";
     private final CustomerService customerService;
-
-    public CustomerController(CustomerService customerService) {
-        this.customerService = customerService;
-    }
 
     @GetMapping("/{customerId}")
     public ResponseEntity<CustomerDto> getCustomer(@PathVariable UUID customerId) {
@@ -28,8 +27,8 @@ public class CustomerController {
 
     @PostMapping
     public ResponseEntity handleCreate(@Valid @RequestBody CustomerDto customerDto){
-        CustomerDto customerDtoCreated =  customerService.create(customerDto);
-        Headers headers = new Headers();
+        val customerDtoCreated =  customerService.create(customerDto);
+        val headers = new Headers();
         headers.add("Location", BASE_URL + "/" + customerDtoCreated.getId().toString());
         return new ResponseEntity(headers, HttpStatus.CREATED);
     }
