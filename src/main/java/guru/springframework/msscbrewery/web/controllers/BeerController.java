1cb6f65cb651542be08a76457b6ff2ec686f4308
Jean Griesbacher <jnsmplcpt@gmail.com>
2020-05-21 17:35:05 +0200

    Validation error handling

--- src/main/java/guru/springframework/msscbrewery/web/controllers/BeerController.java
+++ src/main/java/guru/springframework/msscbrewery/web/controllers/BeerController.java
@@ -7,7 +7,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.ConstraintViolationException;
 import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -46,5 +49,14 @@ public class BeerController {
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void handleDelete(@PathVariable UUID beerId){
         beerService.deleteById(beerId);
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<List> handleConstraintValidation(ConstraintViolationException e){
+        List<String> errors = new ArrayList<>(e.getConstraintViolations().size());
+        e.getConstraintViolations().stream().forEach(ex -> {
+            errors.add(ex.getPropertyPath() + ":"+ex.getMessage());
+        });
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
     }
 }
